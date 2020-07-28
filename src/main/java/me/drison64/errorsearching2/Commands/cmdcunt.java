a2734eaad213771ee080df322326755381350a9e
Drison64 <Drison64@seznam.cz>
2020-07-28 23:10:24 +0200

    label -> s

--- src/main/java/me/drison64/errorsearching2/Commands/cmdcunt.java
+++ src/main/java/me/drison64/errorsearching2/Commands/cmdcunt.java
@@ -5,8 +5,9 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
 public class cmdcunt implements CommandExecutor {
+
     @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+    public boolean onCommand(CommandSender sender, Command command, String s, String[] args) {
         sender.sendMessage("cockity cock");
         return false;
     }
